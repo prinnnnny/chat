a2d8b931d71442c10821c60a4b575e1120f6b22b
prinnnnny <prinnnnny@gmail.com>
2016-07-03 17:25:46 +0100

    Working UI

--- chatClient.py
+++ chatClient.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python
 
-import socket, argparse, sys, ssl, hashlib, os, curses
+import socket, argparse, sys, ssl, hashlib, os, curses, readline, traceback
 
 class ChatClient(object):
-	def __init__(self, server, port, username):
-
+	def __init__(self):
 		#Create master socket
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -12,21 +11,12 @@ class ChatClient(object):
 		self.inputs = [self.sock, sys.stdin] #Inputs to read from	
 
 		#Create SSL wrapper for socket
-		self.ssl_key = "ssl_key"
-		self.ssl_cert = "ssl_cert"
+		self.ssl_key = "../ssl_key"
+		self.ssl_cert = "../ssl_cert"
 		self.sock = ssl.wrap_socket(self.sock,server_side=False,certfile=self.ssl_cert,keyfile=self.ssl_key, ssl_version=ssl.PROTOCOL_TLSv1)
-		
-		#Grab server details from the args
-		self.server = server
-		self.port = port
-		self.username = username
 
-		try:
-			self.sock.connect((self.server, int(self.port))) #Connect to server on specified port
-			print 'Connected to %s on port %d' % (self.server, self.port)
-		except socket.error as err:
-			print 'Failed to connect to server'
-			sys.exit(0)
+		#Grab server details from the args
+		self.screen_pointer = 10
 
 	def start(self):
 		self.draw_menu()
@@ -40,8 +30,8 @@ class ChatClient(object):
 				break
 			else:
 				resp += resp
-		print resp + '\n'
-		
+				print resp + '\n'
+
 	def start_room(self):
 		print 'Create a new chat room'
 		room_name = raw_input('Please enter room name: ')
@@ -52,27 +42,84 @@ class ChatClient(object):
 			self.sock.send()
 		else:
 			self.sock.send() #place holder for room name and password
-			
+
+	def select_username(self):
+		screen = curses.initscr()
+		scr_size = screen.getmaxyx()
+		screen.clear()
+		screen.addstr(2, 2, "Please enter a username below (Max len 10 chars)")
+		tbox = curses.newwin(3,scr_size[1]-4, 4,4)
+		tbox.box()
+		tbox.addstr(1,1, 'Username: ')
+		screen.refresh()
+		tbox.refresh()
+		username = tbox.getstr(1, len('Username: ')+1, 20)
+		return username
+
+	def get_server_ip(self):
+		try:
+			screen = curses.initscr()
+			scr_size = screen.getmaxyx()
+			screen.clear()
+
+			#Get server IP info from input
+			screen.addstr(2, 2, "Please enter server IP")
+			tbox = curses.newwin(3,scr_size[1]-4, 4,4)
+			tbox.box()
+			tbox.addstr(1,1, 'Server IP: ')
+			screen.refresh()
+			tbox.refresh()
+			self.server_ip = tbox.getstr(1, len('Server IP: ')+1, 20)
+			self.get_server_port()
+		except:
+			curses.endwin()
+
+	
+	def get_server_port(self):
+		try:
+			screen = curses.initscr()
+			scr_size = screen.getmaxyx()
+			screen.clear()
+
+			#Get server port info from input
+			screen.addstr(2, 2, "Please enter port to connect")
+			tbox = curses.newwin(3,scr_size[1]-4, 4,4)
+			tbox.box()
+			tbox.addstr(1,1, 'Port: ')
+			screen.refresh()
+			tbox.refresh()
+			self.server_port = tbox.getstr(1, len('Port: ')+1, 5)
+		except:
+			curses.endwin()
+
+	def server_connect(self, ip, port):
+		try:
+			self.sock.connect((ip, port)) #Connect to server on specified port
+		except socket.error as err:
+			print 'Failed to connect to server'
+			sys.exit(0)
+
 	def draw_menu(self):
 		try:
 			x = 0
-			while x != ord('4'):
+			while x != ord('7'):
 				self.screen = curses.initscr()
 				self.screen.keypad(1)
-				scr_size = self.screen.getmaxyx()
+				self.scr_size = self.screen.getmaxyx()
 				self.screen.clear()
 				self.screen.border(0)
 				self.screen.addstr(2, 2, "Please choose an option below")
-				self.screen.addstr(4, 4, "[1] Join chat room")
-				self.screen.addstr(5, 4, "[2] Start new private chat")
-				self.screen.addstr(6, 4, "[3] List chat rooms")
-				self.screen.addstr(7, 4, "[4] List connected users")
-				self.screen.addstr(8, 4, "[5] Exit")
+				self.screen.addstr(4, 4, "[1] Connect to server")
+				self.screen.addstr(5, 4, "[2] Join chat room")
+				self.screen.addstr(6, 4, "[3] Start new private chat")
+				self.screen.addstr(7, 4, "[4] List chat rooms")
+				self.screen.addstr(8, 4, "[5] List connected users")
+				self.screen.addstr(9, 4, "[6] Enter username")
+				self.screen.addstr(10, 4, "[7] Exit")
 				self.screen.refresh()
 				x = self.screen.getch() #Get key press
 				if x == ord('1'):
-					pass
-					#self.join_room()
+					self.get_server_ip()
 				elif x == ord('2'):
 					#self.start_chat()
 					pass
@@ -83,14 +130,22 @@ class ChatClient(object):
 					#self.list_users()
 					pass
 				elif x == ord('5'):
+					#self.list_rooms()
+					pass
+				elif x == ord('6'):
+					self.username = self.select_username()
+				elif x == ord('7'):
 					curses.endwin()
 					sys.exit()
-	
+		except:
+			#curses.endwin()
+			print(traceback.format_exc())
+
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
-	parser.add_argument('-u', '--username', help='Specify username', action='store', dest='username', required=True)
-	parser.add_argument('-s', '--server', help='Specify server IP address', action='store', dest='server', required=True)
-	parser.add_argument('-p', '--port', help='Specify server port', action='store', dest='port', required=True)
+	parser.add_argument('-u', '--username', help='Specify username', action='store', dest='username', required=False)
+	parser.add_argument('-s', '--server', help='Specify server IP address', action='store', dest='server', required=False)
+	parser.add_argument('-p', '--port', help='Specify server port', action='store', dest='port', required=False)
 	args = parser.parse_args()
-	client = ChatClient(args.server, args.port, args.username)
+	client = ChatClient()
 	client.draw_menu()
---
+++ client.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+class Client(object):
+	def __init__(self, username, socket):
+		self.username = username
+		self.socket = socket
